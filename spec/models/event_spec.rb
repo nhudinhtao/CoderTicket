efbc002e394030bb6e5d22deb1160c06bb1b915c
Tao Nhu <tao.nhu@sugar.sg>
2016-10-27 13:51:43 +0700

    implement upcoming event

--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,6 +1,20 @@
 require 'rails_helper'
 
+def event_link
+  page.all(:css, "h4.card-title").map(&:text)
+end
+
 RSpec.describe Event, type: :model do
   
+  it "show upcoming event" do
+    # create new event and it will show on page
+    venue1 = Venue.create!(name: "Da lat")
+    category1 = Category.create!(name: "Entertainment")
+    event1 = Event.create!(name: "Event 1", extended_html_description: "<p>this is a test event</p>", venue: venue1, category: category1,  starts_at: Time.new(2016, 12, 24, 01, 04, 44))
+
+    expect(Event.upcoming).to eq [event1]
+
+  end
   
 end
+
