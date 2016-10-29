0ce0d8cf7339de678dc955feffcc4606227a92b0
Tao Nhu <tao.nhu@sugar.sg>
2016-10-29 12:21:32 +0700

    Users cannot buy more tickets than the quantity available

--- README.md
+++ README.md
@@ -27,7 +27,7 @@ The person with best test coverage will win a small present from the instructor 
 
 * [x] Each event can have multiple types of tickets, each with a different `price` and `max_quantity`.
 * [x] Users can buy tickets to an event, choose the types of tickets, and the quantity of tickets.
-* [ ] Users cannot buy more tickets than the quantity available.
+* [x] Users cannot buy more tickets than the quantity available.
 * [ ] Users can only buy up to 10 of a ticket type at a time. Show a nice flash message.
 
 #### Users
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,6 +14,12 @@ class OrdersController < ApplicationController
     @quantities.each do |ticketType, quantity|
       if quantity.to_i > 0
         ticketType = TicketType.find(ticketType)
+        
+        if (quantity.to_i > ticketType.max_quantity)
+          flash[:error] = 'Cannot buy ' + ticketType.name + ', there are only ' + ticketType.max_quantity.to_s + ' tickets.'
+          redirect_to new_event_ticket_path(ticketType.event) and return
+        end
+
         orderItem = OrderItem.new(order: @order, ticket_type: ticketType, quantity: quantity.to_i) 
         orderItem.save
       end
